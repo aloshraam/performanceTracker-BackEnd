356dd9fcad24e99b411fe08c9aa3ce189d4c09ad
aloshraam <aloshraam@gmail.com>
2025-04-01 16:20:29 +0530

    attedance added

---
+++ adminapi/serializer.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+from hrapi.models import *
+from rest_framework import serializers
+from .models import Attendance
+
+
+
+class HrSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Hr
+        fields=["id","name","email_address","phoneno","home_address","job_title","position","department","prefferred_timezone","linkedin_profile","skills","certification","experience","is_adminapproved"]
+        
+
+class TeamleadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=TeamLead
+        fields=["id","name","email_address","phoneno","home_address","job_title","position","department","prefferred_timezone","linkedin_profile","skills","certification","experience"]
+    
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Employee
+        fields=["id","name","email_address","phoneno","home_address","job_title","department","linkedin_profile","manager_name","resume","start_date","in_team"]        
+       
+
+class MeetingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Meeting
+        fields=["title","link","date","time"]
+        
+
+class MeetingListSerializer(serializers.ModelSerializer):
+    organizer=serializers.CharField(read_only=True)
+    class Meta:
+        model=Meeting
+        fields="__all__"
+        
+
+class TechnologiesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=TechnologiesList
+        fields="__all__"
+
+
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+
+    class Meta:
+        model = Attendance
+        fields = ["user", "date", "login_time", "status"]
